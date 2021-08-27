5a5736dfd4607a068d1a16cd053860afb102aa97
Rita Kolotilova <margarita.kolotilova@jetbrains.com>
2021-08-27 08:41:50 +0000

    First automation job

---
+++ .space.kts
@@ -0,0 +1,19 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Hello World!") {
+    container(displayName = "Say Hello", image = "hello-world")
+}
+job("Notification on developer branch update") {
+    container() {
+        kotlinScript { api ->
+            val recipient = MessageRecipient.Channel(ChatChannel.FromName("devs"))
+            val content = ChatMessage.Text("Some message text!")
+            api.space().chats.messages.sendMessage(recipient, content)
+
+        }
+    }
+}
